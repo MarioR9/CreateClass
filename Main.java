a9e1e08aeb3269f437dee85049d93738f5b97d78
MarioR9 <mr20620@email.vccs.edu>
2017-12-09 00:11:20 -0500

    First commit

--- Main.java
+++ Main.java
@@ -7,34 +7,34 @@ public class Main {
         String name = keyboard.next();
         String outputAge;
         String outputName;
-        System.out.println("Enter person 1's age");
+        System.out.println("Enter First person age");
         int age = Integer.parseInt(keyboard.next());
         MainDriver person1 = new  MainDriver(name,age);
         System.out.println(person1);
-        System.out.println("Enter Person 2's name");
+        System.out.println("Enter second person name");
         name = keyboard.next();
-        System.out.println("Enter Person 2's age");
+        System.out.println("Enter second person age");
         age = Integer.parseInt(keyboard.next());
         MainDriver person2 = new  MainDriver(name,age);
         System.out.println(person2);
         if (person1.toString().equals(person2.toString())==true){
-            System.out.println("Person 1 equals to Person 2");
+            System.out.println("first person equals to second person");
         } else {
-            System.out.println("Person 1 does not equal to Person 2");
+            System.out.println("first person does not equal to second person");
         }
         if (person1.getName().equals(person2.getName())){
-            System.out.println("Person 1 does have the same name as Person 2");
+            System.out.println("first person does have the same name as second person");
         } else {
-            System.out.println("Person 1 does not have the same name as Person 2");
+            System.out.println("first person does not have the same name as second person");
         }
         if (person1.getAge()==person2.getAge()){
-            outputAge ="Person 1 is the same age as Person 2";
+            outputAge ="first person is the same age as second person";
         }
         else if (person1.getAge()>person2.getAge()) {
-            outputAge="Person 1 is older than Person 2";
+            outputAge="First person is older than second person";
         }
         else {
-            outputAge="Person 1 is younger than Person 2";
+            outputAge="first person is younger than second person";
         }
         System.out.println(outputAge);
     }
